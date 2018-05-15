bedccade17734ef873cbe197467c666334db8e2a
Mitchell Rosen <mitchellwrosen@gmail.com>
2018-05-14 22:40:12 -0400

    Add docker gc

--- Main.hs
+++ Main.hs
@@ -1,3 +1,5 @@
+{-# language LambdaCase #-}
+
 import Control.Monad
 import Options.Applicative
 import System.Process
@@ -26,12 +28,49 @@ parser :: Parser (IO ())
 parser =
   hsubparser
     (mconcat
-      [ subcommand "pid" pidParser pidInfo
+      [ subcommand "docker" dockerParser dockerInfo
+      , subcommand "pid" pidParser pidInfo
       ])
 
 subcommand :: String -> Parser a -> InfoMod a -> Mod CommandFields a
 subcommand n p m =
   command n (info p m)
+
+--------------------------------------------------------------------------------
+-- docker
+--------------------------------------------------------------------------------
+
+dockerParser :: Parser (IO ())
+dockerParser =
+  hsubparser
+    (mconcat
+      [ subcommand "gc" dockerGcParser dockerGcInfo
+      ])
+
+dockerInfo :: InfoMod (IO ())
+dockerInfo =
+  progDesc "Docker porcelain"
+
+dockerGcParser :: Parser (IO ())
+dockerGcParser =
+  pure $ do
+    putStrLn "# Removing exited containers"
+    readCreateProcess (shell "docker ps -aq --filter=status=exited") "" >>= \case
+      "" ->
+        pure ()
+      ids ->
+        callCommand ("docker rm " ++ unwords (lines ids))
+
+    putStrLn "# Removing dangling images"
+    readCreateProcess (shell "docker images -q --filter=dangling=true") "" >>= \case
+      "" ->
+        pure ()
+      ids ->
+        callCommand ("docker rmi " ++ unwords (lines ids))
+
+dockerGcInfo :: InfoMod (IO ())
+dockerGcInfo =
+  progDesc "Garbage collect stopped containers and dangling images"
 
 --------------------------------------------------------------------------------
 -- pid
@@ -44,4 +83,4 @@ pidParser =
 
 pidInfo :: InfoMod (IO ())
 pidInfo =
-  mempty
+  progDesc "Find the pid of a running process"
